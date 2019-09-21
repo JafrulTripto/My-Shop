b005a2391f8fdb8cfa6daac15b584e16f8ef934b
Jafrul Tripto <tripto_ewu@outlook.com>
2019-09-22 00:00:58 +0600

    Dev added to Master Branch

--- src/Store/Actions/index.js
+++ src/Store/Actions/index.js
@@ -4,13 +4,8 @@ import Axios from 'axios';
 
 export const fetchProductUnit = () => {
     return async (dispatch) => {
-<<<<<<< HEAD
-        await Axios.get(process.env.REACT_APP_SERVER+'getUnit').then(response => {
-            dispatch({ type : 'FETCH_UNITS', payload: response.data})
-=======
         await Axios.get(process.env.REACT_APP_SERVER + 'getUnit').then(response => {
             dispatch({ type: 'FETCH_UNITS', payload: response.data })
->>>>>>> dev
         }).catch(err => {
             console.log(err);
         })
@@ -20,13 +15,8 @@ export const fetchProductUnit = () => {
 
 export const fetchProductCategory = () => {
     return async (dispatch) => {
-<<<<<<< HEAD
-        await Axios.get(process.env.REACT_APP_SERVER+'getCategory').then(response => {
-            dispatch({ type : 'FETCH_CATEGORY', payload: response.data})
-=======
         await Axios.get(process.env.REACT_APP_SERVER + 'getCategory').then(response => {
             dispatch({ type: 'FETCH_CATEGORY', payload: response.data })
->>>>>>> dev
         }).catch(err => {
             console.log(err);
         })
@@ -35,13 +25,8 @@ export const fetchProductCategory = () => {
 }
 export const fetchSuppliers = () => {
     return async (dispatch) => {
-<<<<<<< HEAD
-        await Axios.get(process.env.REACT_APP_SERVER+'getSuppliers').then(response => {
-            dispatch({ type : 'FETCH_SUPPLIER', payload: response.data})
-=======
         await Axios.get(process.env.REACT_APP_SERVER + 'getSuppliers').then(response => {
             dispatch({ type: 'FETCH_SUPPLIER', payload: response.data })
->>>>>>> dev
         }).catch(err => {
             console.log(err);
         })
